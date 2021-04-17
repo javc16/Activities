e594d2bc9dd774623b838a89d1fc9a919f929a1b
DESKTOP-AICR3S2\alexe <javc1603@gmail.com>
2021-04-17 14:04:33 -0600

    adding context class

---
+++ src/BackEnd-Activitites/Context/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndActivitites.Context
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+
+       
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Server=DESKTOP-AICR3S2;DATABASE=Test;Trusted_Connection=True");
+        }
+    }
+}
